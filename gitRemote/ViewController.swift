46980233f99daa03368adb76d4733741014338b4
CuGi <ji3g4m6zo6@msn.com>
2016-08-22 14:12:22 +0800

    Initial Commit

---
+++ gitRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitRemote
+//
+//  Created by CuGi on 2016/8/22.
+//  Copyright © 2016年 Cu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
